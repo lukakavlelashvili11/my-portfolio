9e3cc291cd45c35fe9ed5b657048f4bd92a66ab3
luka <lukakavlela20@gmail.com>
2021-09-06 00:50:42 +0400

    start front

---
+++ front/src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+      <div class="header">
+          <ul class="nav">
+              <li>
+                  <router-link to="/">About me</router-link>
+              </li>
+              <li>Skills</li>
+              <li>Projects</li>
+              <li>Contact</li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+.header{
+    max-width: 100%;
+    display: flex;
+    justify-content: flex-end;
+}
+.nav{
+    list-style: none;
+    padding: 0px;
+    display: flex;
+    justify-content: space-between;
+}
+.nav li{
+    padding: 10px;
+    background: linear-gradient(to right,#8EC5FC,#E0C3FC);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 20px;
+    font-family: 'Roboto', sans-serif;
+}
+.nav li a{
+    text-decoration: none;
+}
+</style>
---
+++ front/src/views/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <div class="name">
+          <span>Hi there! i'm</span>
+          <h1>Luka</h1>
+          <h1>Kavlelashvili</h1>
+          <span>The full stack web developer</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.name h1,span{
+    /* background: linear-gradient(to right,#8EC5FC,#E0C3FC);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent; */
+    color: yellow;
+    line-height: 20px;
+}
+</style>
